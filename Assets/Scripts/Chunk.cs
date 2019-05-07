069debb3ca3fac67454b3e1010b3cd81ecb6b2c7
Marco Pleines <m.pleines@devbeyond.de>
2019-05-07 10:59:52 +0200

    Added comments to chunk.cs

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -3,13 +3,23 @@ using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
+/// <summary>
+/// The serializable BlockData class contains block information that are to be saved to a file.
+/// </summary>
 [Serializable]
 class BlockData
 {
 	public Block.BlockType[,,] matrix;
 	
+    /// <summary>
+    /// Empty constructor
+    /// </summary>
 	public BlockData(){}
 
+    /// <summary>
+    /// The constrcutor initializes its matrix for storing all blocks of the given chunk.
+    /// </summary>
+    /// <param name="b">3D block array (i.e. chunk)</param>
 	public BlockData(Block[,,] b)
 	{
 		matrix = new Block.BlockType[World.chunkSize,World.chunkSize,World.chunkSize];
@@ -22,20 +32,33 @@ class BlockData
 	}
 }
 
+/// <summary>
+/// Chunk class that takes care of storing the information of the chunk's blocks.
+/// It renders the chunk and provides functionality for saving, loading and updating the chunk.
+/// </summary>
 public class Chunk
 {
-	public Material cubeMaterial;
-	public Material fluidMaterial;
-	public Block[,,] chunkData;
-	public GameObject chunk;
-	public GameObject fluid;
-	public enum ChunkStatus {DRAW,DONE,KEEP};
-	public ChunkStatus status;
-	public ChunkMB mb;
-	BlockData bd;
-	public bool changed = false;
-	bool treesCreated = false;
+	public Material cubeMaterial;   // Materia for solid blocks
+	public Material fluidMaterial;  // Material for transparent blocks
+	public Block[,,] chunkData;     // 3D Array containing all blocks of the chunk
+	public GameObject chunk;        // GameObject that holds the mesh of the solid parts of the chunk
+	public GameObject fluid;        // GameObject that holds the mesh of the transparent parts, like water, of the chunk
+	public enum ChunkStatus
+    {
+        DRAW,                       // DRAW: data of the chunk has been created and needs to be rendered next
+        DONE                        // DONE: Trees have been built and the chunk has been rendered
+    };
+	public ChunkStatus status;      // Current state of the chunk
+	public ChunkMB mb;              // The MonoBehaviour of the Chunk
+	BlockData bd;                   // 
+	public bool changed = false;    // If a chunk got modified (e.g. a block got destroyed by the player), set this to true to redraw the chunk upon the next update.
+	bool treesCreated = false;      // 
 
+    /// <summary>
+    /// Creates a file name for the to be saved or loaded chunk based on its position. On Windows machines the data is saved in AppData\LocalLow\DefaultCompany.
+    /// </summary>
+    /// <param name="v">Position of the chunk</param>
+    /// <returns>Returns the file name of the to be saved or loaded chunk</returns>
 	string BuildChunkFileName(Vector3 v)
 	{
 		return Application.persistentDataPath + "/savedata/Chunk_" + 
@@ -47,7 +70,11 @@ public class Chunk
 										".dat";
 	}
 
-	bool Load() //read data from file
+    /// <summary>
+    /// Loads chunk data from file.
+    /// </summary>
+    /// <returns>Returns true if the file to be loaded exists</returns>
+	private bool Load()
 	{
 		string chunkFile = BuildChunkFileName(chunk.transform.position);
 		if(File.Exists(chunkFile))
@@ -57,13 +84,15 @@ public class Chunk
 			bd = new BlockData();
 			bd = (BlockData) bf.Deserialize(file);
 			file.Close();
-			//Debug.Log("Loading chunk from file: " + chunkFile);
 			return true;
 		}
 		return false;
 	}
 
-	public void Save() //write data to file
+    /// <summary>
+    /// Writes chunk data to file.
+    /// </summary>
+	public void Save()
 	{
 		string chunkFile = BuildChunkFileName(chunk.transform.position);
 		
@@ -76,9 +105,11 @@ public class Chunk
 		bd = new BlockData(chunkData);
 		bf.Serialize(file, bd);
 		file.Close();
-		//Debug.Log("Saving chunk from file: " + chunkFile);
 	}
 
+    /// <summary>
+    /// If a block was destroyed upon player interaction, trigger the process of dropping sand for each sand block.
+    /// </summary>
 	public void UpdateChunk()
 	{
 		for(int z = 0; z < World.chunkSize; z++)
@@ -92,15 +123,18 @@ public class Chunk
 										20));
 					}
 				}
-
 	}
 
-	void BuildChunk()
+    /// <summary>
+    /// Builds the chunk from scatch or loads it from file. This functions does not draw the chunk.
+    /// </summary>
+	private void BuildChunk()
 	{
 		bool dataFromFile = false;
-		//dataFromFile = Load();
+        // Commented load functionality, because this may cause issues while changing the underlying code (saved files may not represent the current state of the project)
+        //dataFromFile = Load();
 
-		chunkData = new Block[World.chunkSize,World.chunkSize,World.chunkSize];
+        chunkData = new Block[World.chunkSize,World.chunkSize,World.chunkSize];
 		for(int z = 0; z < World.chunkSize; z++)
 			for(int y = 0; y < World.chunkSize; y++)
 				for(int x = 0; x < World.chunkSize; x++)	
@@ -110,6 +144,7 @@ public class Chunk
 					int worldY = (int)(y + chunk.transform.position.y);
 					int worldZ = (int)(z + chunk.transform.position.z);
 
+                    // Load chunk from file
 					if(dataFromFile)
 					{
 						chunkData[x,y,z] = new Block(bd.matrix[x, y, z], pos, 
@@ -117,13 +152,13 @@ public class Chunk
 						continue;
 					}
 
-
 					int surfaceHeight = Utils.GenerateHeight(worldX,worldZ);
 					
-					
+                    // Place bedrock at height 0
 					if(worldY == 0)
 						chunkData[x,y,z] = new Block(Block.BlockType.BEDROCK, pos, 
 						                chunk.gameObject, this);
+                    // Place Diamond, Redstone or Stone at certain heights and probabilities
 					else if(worldY <= Utils.GenerateStoneHeight(worldX,worldZ))
 					{
 						if(Utils.fBM3D(worldX, worldY, worldZ, 0.01f, 2) < 0.4f && worldY < 40)
@@ -136,6 +171,7 @@ public class Chunk
 							chunkData[x,y,z] = new Block(Block.BlockType.STONE, pos, 
 						                chunk.gameObject, this);
 					}
+                    // Place trunks of a tree or grass blocks on the surface
 					else if(worldY == surfaceHeight)
 					{
 						if(Utils.fBM3D(worldX, worldY, worldZ, 0.4f, 2) < 0.4f)
@@ -145,28 +181,33 @@ public class Chunk
 							chunkData[x,y,z] = new Block(Block.BlockType.GRASS, pos, 
 						                chunk.gameObject, this);
 					}
+                    // Place dirt blocks
 					else if(worldY < surfaceHeight)
 						chunkData[x,y,z] = new Block(Block.BlockType.DIRT, pos, 
 						                chunk.gameObject, this);
+                    // Place water blocks below height 65
 					else if(worldY < 65)
 						chunkData[x,y,z] = new Block(Block.BlockType.WATER, pos, 
 						                fluid.gameObject, this);
+                    // Place air blocks
 					else
 					{
 						chunkData[x,y,z] = new Block(Block.BlockType.AIR, pos, 
 						                chunk.gameObject, this);
 					}
 
+                    // Create caves
 					if(chunkData[x,y,z].blockType != Block.BlockType.WATER && Utils.fBM3D(worldX, worldY, worldZ, 0.1f, 3) < 0.42f)
 						chunkData[x,y,z] = new Block(Block.BlockType.AIR, pos, 
 						                chunk.gameObject, this);
 
-
 					status = ChunkStatus.DRAW;
-
 				}
 	}
 
+    /// <summary>
+    /// Redraws this chunk by destroying all mesh and collision components and then creating new ones.
+    /// </summary>
 	public void Redraw()
 	{
 		GameObject.DestroyImmediate(chunk.GetComponent<MeshFilter>());
@@ -178,6 +219,10 @@ public class Chunk
 		DrawChunk();
 	}
 
+    /// <summary>
+    /// Draws the chunk. If trees are not created yet, create them.
+    /// The draw process creates meshes for all blocks and then combines them to a solid and a transparent mesh.
+    /// </summary>
 	public void DrawChunk()
 	{
 		if(!treesCreated)
@@ -196,16 +241,29 @@ public class Chunk
 				{
 					chunkData[x,y,z].Draw();
 				}
+
+        // Prepare solid chunk mesh
 		CombineQuads(chunk.gameObject, cubeMaterial);
 		MeshCollider collider = chunk.gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
 		collider.sharedMesh = chunk.transform.GetComponent<MeshFilter>().mesh;
 
+        // Prepare transparent chunk mesh
 		CombineQuads(fluid.gameObject, fluidMaterial);
+
 		status = ChunkStatus.DONE;
 	}
 
-	void BuildTrees(Block trunk, int x, int y, int z)
+    /// <summary>
+    /// Trunks are already place within the BuildChunk method.
+    /// For each trunk, build a tree.
+    /// </summary>
+    /// <param name="trunk">Woodbase block as a trunk of the to be created tree</param>
+    /// <param name="x">x position of the block</param>
+    /// <param name="y">y position of the block</param>
+    /// <param name="z">z position of the block</param>
+	private void BuildTrees(Block trunk, int x, int y, int z)
 	{
+        // Do not build a tree if there is no woodbase
 		if(trunk.blockType != Block.BlockType.WOODBASE) return;
 
 		Block t = trunk.GetBlock(x, y+1, z);
@@ -238,26 +296,35 @@ public class Chunk
 		}
 	}
 
+    /// <summary>
+    /// Empty constructor.
+    /// </summary>
 	public Chunk(){}
-	// Use this for initialization
-	public Chunk (Vector3 position, Material c, Material t) {
-		
-		chunk = new GameObject(World.BuildChunkName(position));
+
+	/// <summary>
+    /// Initializes a chunk by providing a position, a material for blocks and a material for partially transparent blocks.
+    /// </summary>
+    /// <param name="position">Position of the chunk</param>
+    /// <param name="c">The material for the solid blocks of the chunk</param>
+    /// <param name="t">The material for the transparent blocks of the chunk</param>
+	public Chunk (Vector3 position, Material c, Material t)
+    {
+        // Create GameObjects holding the chunk's meshes
+		chunk = new GameObject(World.BuildChunkName(position));         // solid chunk mesh, e.g. dirt blocks
 		chunk.transform.position = position;
-		fluid = new GameObject(World.BuildChunkName(position)+"_F");
+		fluid = new GameObject(World.BuildChunkName(position)+"_F");    // transparent chunk mesh, e.g. water blocks
 		fluid.transform.position = position;
 
-		mb = chunk.AddComponent<ChunkMB>();
+		mb = chunk.AddComponent<ChunkMB>();                             // Adds the chunk's Monobehaviour
 		mb.SetOwner(this);
 		cubeMaterial = c;
 		fluidMaterial = t;
-		BuildChunk();
+		BuildChunk();                                                   // Start building the chunk
 	}
-
 	
 	public void CombineQuads(GameObject o, Material m)
 	{
-		//1. Combine all children meshes
+		// 1. Combine all children meshes
 		MeshFilter[] meshFilters = o.GetComponentsInChildren<MeshFilter>();
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
         int i = 0;
@@ -267,22 +334,20 @@ public class Chunk
             i++;
         }
 
-        //2. Create a new mesh on the parent object
+        // 2. Create a new mesh on the parent object
         MeshFilter mf = (MeshFilter) o.gameObject.AddComponent(typeof(MeshFilter));
         mf.mesh = new Mesh();
 
-        //3. Add combined meshes on children as the parent's mesh
+        // 3. Add combined meshes on children as the parent's mesh
         mf.mesh.CombineMeshes(combine);
 
-        //4. Create a renderer for the parent
+        // 4. Create a renderer for the parent
 		MeshRenderer renderer = o.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
 		renderer.material = m;
 
-		//5. Delete all uncombined children
+		// 5. Delete all uncombined children
 		foreach (Transform quad in o.transform) {
      		GameObject.Destroy(quad.gameObject);
  		}
-
 	}
-
 }
